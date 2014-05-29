280d7b50e0530a1b22426f655e18dd2323035edc
saleemayman <aiman1175@gmail.com>
2014-05-29 14:33:36 +0200

    assignment_05 complete; accepted :)

--- Assignment_05/Assignment_02/prime_count_par.c
+++ Assignment_05/Assignment_02/prime_count_par.c
@@ -1,123 +1,54 @@
-#include <math.h>
-#include <pthread.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include "stdio.h"
+#include <omp.h>
 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-unsigned long tmp_start;
-unsigned long tmp_end;
-
-struct pthread_args
+int prime_check(unsigned long number)
 {
-	unsigned long start, end, out, chunk_size, a, b;
-};
+    unsigned long i, sqrRoot;
+    int check;
 
-void * check_is_prime(void *ptr)	{
-	struct pthread_args *arg = ptr;
-	long N, j;
-	unsigned long sqrRoot, endVal;
-	int check;
-	endVal = arg->b;
+    check = 1;
 
-	arg->out = 0;
-
-	/*pthread_mutex_lock(&mutex);
-	tmp_start = tmp_end + arg->a;
-	tmp_end = tmp_start + arg->chunk_size - 1;
-
-	arg->start = tmp_start;
-	arg->end = tmp_end;
-	pthread_mutex_unlock(&mutex);*/
-	if ((arg->b - arg->a) < arg->chunk_size)
-	{
-		arg->chunk_size = (arg->b - arg->a);
-	}
-
-	/*printf("Before while. start: %lu, end: %lu, a: %lu, b: %lu \n", tmp_start, tmp_end, arg->a, arg->b);*/
-
-	/* ----- ----- -----, ----- ----- -----, ----- ----- -----, ---- */
-	while (tmp_end <= endVal)
-	{
-		pthread_mutex_lock(&mutex);
-		tmp_start = tmp_end + 1;
-		tmp_end = tmp_start + arg->chunk_size - 1;
-
-		arg->start = tmp_start;
-		arg->end = tmp_end;
-		pthread_mutex_unlock(&mutex);
-
-		/*printf("N1: %lu, endVal: %lu \n",N, endVal);*/
-		for (N = arg->start; N <= arg->end; N++)
-		{
-			/*printf("start: %lu, end: %lu, N: %lu, endVal: %lu \n",arg->start, arg->end,  N, endVal);*/
-			check = 1;
-			sqrRoot = sqrt(N) + 1;
-			for (j = 2; j <= sqrRoot; j++)
-			{
-				if ((N % j) == 0)
-				{
-					check = 0;
-					break;
-				}
-			}
-			if (check == 1)
-			{
-				arg->out++;
-			}
-		}
-			/*pthread_mutex_lock(&mutex);
-			tmp_start = tmp_end + 1;
-			tmp_end = tmp_start + arg->chunk_size - 1;
-
-			arg->start = tmp_start;
-			arg->end = tmp_end;
-			pthread_mutex_unlock(&mutex);*/
-
-
-
-	}
-	/*printf("End___ while. start: %lu, end: %lu, a: %lu, b: %lu \n", tmp_start, tmp_end, arg->a, arg->b);*/
-	return NULL;
+    /* 	ignore zero and one, but count two*/
+    if(number == 0 || number == 1)
+    {
+        check = 0;
+    }
+    else if(number == 2)
+    {
+        check = 1;
+    }
+    else
+    {
+    	sqrRoot = sqrt(number) + 1;
+        for(i = 2; i <= sqrRoot; i++)
+        {
+            if((number % i) == 0)
+            {
+                check = 0;
+                break;
+            }
+        }
+    }
+    return check;
 }
-
 
 unsigned long prime_count(unsigned long a, unsigned long b, unsigned long num_threads, unsigned long chunk_size)
 {
-	long j;
-	unsigned long count = 0;
+	/*printf("prime_count_seq started ....\n");*/
+	long N;
+	unsigned long sum = 0;
 
-	tmp_start = 0;
-	tmp_end = a;
+	omp_set_num_threads(num_threads);	/* Launch threaaaaaaaaaadsssssssss!!	*/
 
-	pthread_t *thread;
-	thread = malloc(num_threads * sizeof (*thread));
-	struct pthread_args *thread_arg;
-	thread_arg = malloc(num_threads * sizeof (*thread_arg));
-
-	for (j = 0; j < num_threads; j++)
+#pragma omp parallel
+{
+	#pragma omp for schedule(dynamic, chunk_size) reduction(+:sum) 
+	for (N = a; N <= b; N++)
 	{
-		thread_arg[j].a = a;
-		thread_arg[j].b = b;
-		thread_arg[j].chunk_size = chunk_size;
+		sum += prime_check(N);
 	}
-
-	/*printf("prime_count_par start: %lu, end: %lu\n", tmp_start, tmp_end);*/
-	/* Create threads for the computation*/
-	for (j = 0; j < num_threads; j++)
-	{
-		pthread_create(&thread[j], 0, &check_is_prime, &thread_arg[j]);
-	}
-
-	/* Collect data from all the threads	*/
-	for (j = 0; j < num_threads; j++)
-	{
-		pthread_join(thread[j], NULL);
-		count += thread_arg[j].out;
-	}
-
-	free(thread);
-    free(thread_arg);
-
-
-	return count;
+}
+	return sum;
 }
--- Assignment_05/Assignment_02/prime_count_seq.c
+++ Assignment_05/Assignment_02/prime_count_seq.c
@@ -1,30 +1,41 @@
 #include <stdlib.h>
 #include <math.h>
 #include "stdio.h"
+#include <stdbool.h>
 
 unsigned long prime_count(unsigned long a, unsigned long b, unsigned long num_threads, unsigned long chunk_size)
 {
 	/*printf("prime_count_seq started ....\n");*/
 	long N, j;
 	unsigned long sqrRoot, sum = 0;
-	int check;
-
+	bool check;
+/*
+	if (a == 1)
+	{
+		a += 2;
+	}
+*/
 	for (N = a; N <= b; N++)
 	{
-		check = 1;
+		check = true;
 		sqrRoot = sqrt(N) + 1;
+
 		for (j = 2; j <= sqrRoot; j++)
 		{
 			if ((N % j) == 0)
 			{
-				check = 0;
+				check = false;
 				break;
 			}
+			printf("N: %ld \n", N);
 		}
-		if (check == 1)
+
+
+		if (check == true && N != 1)
 		{
 			sum++;
 		}
 	}
+
 	return sum;
 }
